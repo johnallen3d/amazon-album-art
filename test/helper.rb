aa5e061502bb47e05523d7d679c9cdf86ab9510f
John Allen <john@threedogconsulting.com>
2011-01-14 16:13:35 -0500

    re-added helper class after cleanup

---
+++ test/helper.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'bundler'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'amazon-album-art'
+
+class Test::Unit::TestCase
+  def api_key
+    "api-key"
+  end
+  def secret_key
+    "secret-key"
+  end
+end
